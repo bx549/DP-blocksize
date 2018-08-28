ec8a9492f3ab0508d661b1d074ecf93d3453e69c
Darin England <darin.england@gmail.com>
2018-08-28 18:13:26 -0500

    make the state-space course-grained in dp-blocksize.R modify the value iteration algorithm so that the indices of the transition matrix are used and not the state itself. this is necessary for the course-grained state space.

--- dp-blocksize.R
+++ dp-blocksize.R
@@ -2,21 +2,27 @@
 library(tidyverse)
 
 ## state space
-X <- seq(0, 1000, by=1)  # size of mempool
-Y <- seq(0, 100, by=1)   # number of arrivals during prev period
+X <- seq(0, 100000, by=10)  # size of mempool
+Y <- seq(0, 6000, by=10)   # number of arrivals during prev period
+## these numbers were arrived at by looking at the history
+## from aug '17 to aug '18 (which includes the dec/jan busy period)
+## of the
+## mempool tx count (num of txs waiting to be confirmed), and the
+## txs arrival rate (num of txs added to the mempool per second)
+## source: https://www.blockchain.com/charts
 
 ## control is the block size in MB
 ## 1 MB gets 2000 txs
 ## 2 MB gets 4000 txs
 ## and so on
-C <- 1:2  # possible controls
-K <- 100   # txs per unit of control
+C <- 1:3   # possible controls
+K <- 2000  # txs per unit of control
 
 alpha <- .9  # discount factor for DP iteration
 b <- function(x) 15/(1 + exp(-.05*(x-100))) # tx fee in satoshis per byte
 lambda <- .7 # coefficient for AR1 model w_k = lambda*w_{k-1} + xi_k
-sigma <- 75   # sd for error term in AR1 model
-b0 <- 100    # in the AR1 model
+sigma <- sqrt(4.7e6)   # sd for error term in AR1 model
+b0 <- 3000    # in the AR1 model
 
 f <- function(x, y, u) {
     xi <- rnorm(1, 0, sigma)
@@ -48,7 +54,7 @@ g <- b(X) * t(blk.size)  # element-wise
 ## Let's use the system equation for y.next since the state space
 ## for y is smaller than for x. Given y and y.next,
 ## xi = y.next - lambda*y
-## what is the probability that xi took on this value?
+## what is the probability that xi takes on this value?
 ## xi ~ Normal(b0, sigma), so an estimate is provided by
 ## pnorm(xi+.5, mean=b0, sd=sigma) - pnorm(xi-.5, mean=b0, sd=sigma)
 ## Note that the distribution of the random xi does not depend on the
@@ -82,19 +88,24 @@ while (!all(near(V - V.prev, 0, tol=eps))) {
                 x.next <- X[x] - C[u]*K + Y
                 x.next <- ifelse(x.next<0, 0, x.next)
                 x.next <- ifelse(x.next>max(X), max(X), x.next)
-                ## note that x.next is a state, not an index
+                ## note that x.next is a vector of states, not indices
+                ## so we need to determine the associated indices
+                x.next.idx <- match(x.next, X)
                 val[u] <- g[x,u] +
-                    alpha * sum(P[y,Y+1] * diag(V[x.next+1,Y+1]))
+                    alpha * sum(P[y,1:length(Y)] * diag(V[x.next.idx,1:length(Y)]))
             }
             V[x,y] <- max(val)
             ctrl <- which(near(max(val), val))[1]  # could be a tie
             policy[x,y] <- C[ctrl]
         }
     }
-    if (near(k %% 25, 0)) { # print a message to check progress
+    if (near(k %% 10, 0)) { # print a message to check progress
         message("k = ", k, "sum(V-V.prev) = ", sum(V-V.prev))
     }
 }
+
+## save results to disk
+save.image("dp-blocksize.RData")
  
 ## diagnostics and visualization of the optimal value function
 ## and the optimal policy
--- dp.R
+++ dp.R
@@ -24,7 +24,6 @@ g <- array(data = c(2, 1, 0.5, 3),
 alpha <- 0.9   # discount factor
 
 ## applies the DP iteration for state i
-## note: the error bounds are not yet implemented
 FJ <- function(i) {
     cost <- rep(Inf, length(C))   # holds the cost for each control
     for (u in C) {
@@ -35,33 +34,28 @@ FJ <- function(i) {
         cost[u] <- g[i,u] + alpha*cost.to.go
     }
     cost.star <- min(cost)
-    ctrl <- which(near(cost.star, cost)) # store the ctrl that achieves the min cost
+    ctrl <- which(near(cost.star, cost))
+    ## return the min cost and the control that acheived
+    ## the min cost
     list(cost.star, ctrl)
 }
 
 ## apply the value iteration algorithm.
 ## note that this is the gauss-seidel version of value iteration
 ## because we iterate one state at a time and use the interim results,
-## hence the notation FJ(i).
-## if we implment the error bounds then we would need to
-## use the ordinary version of value iteration where we iterate
-## for all states simultaneously, that is TJ(i) if we follow
-## the notation in bertsekas.
-eps <- .0001                      # tolerance for convergence
-k <- 0                            # iteration number
-J <- rep(0, length(S))            # initially, start the value iteration with zero cost
+## hence the notation FJ(i) as in bertsekas.
+eps <- .0001           # tolerance for convergence
+k <- 0                 # iteration number
+J <- rep(0, length(S)) # initially, start the value iteration with zero cost
 J.prev <- rep(eps + 1, length(S)) # to check convergence
 policy <- rep(NA, length(S))
 
 while (!all(near(J - J.prev, 0, tol=eps))) {
     k <- k + 1
     J.prev <- J
-    for (j in 1:length(S)) {
-        lst <- FJ(j)
-        J[j] <- lst[[1]]
-        policy[j] <- lst[[2]]
-        if (near(k %% 100, 0)) { # print a message to check progress
-            message("k = ", k, " J(", j, ") = ", J[j])
-        }
+    for (i in 1:length(S)) {
+        lst <- FJ(i)
+        J[i] <- lst[[1]]
+        policy[i] <- lst[[2]]
     }
 }
